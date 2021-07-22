7a73af69bd1f7229488c94e3130fba1cd89fb6a5
Wesley <wesleysilvadesousa@gmail.com>
2021-07-21 22:06:26 -0300

    feat: search

--- Alg/Controllers/AlgController.cs
+++ Alg/Controllers/AlgController.cs
@@ -19,8 +19,9 @@ namespace Alg.Controllers
         public async Task<ActionResult<Orgao>> Get()
         {
             List<Orgao> orgaos = null;
+            double[] linearArray= new double[50];
+            double[] binaryArray= new double[50];
             var client = new HttpClient();
-            var cod = "01000";
 
             client.DefaultRequestHeaders.Add("chave-api-dados", "4bda51885581e6837f94ba4e6d34e710");
 
@@ -28,75 +29,65 @@ namespace Alg.Controllers
             var streamTask = client.GetStreamAsync("http://api.portaldatransparencia.gov.br/api-de-dados/orgaos-siafi?pagina=1");
 
             orgaos = await JsonSerializer.DeserializeAsync<List<Orgao>>(await streamTask);
-            var watch = System.Diagnostics.Stopwatch.StartNew();
             
-            foreach (Orgao orgao in orgaos)
+            var teste = new Orgao();
+            teste.codigo = "11000";
+            teste.descricao = "Superior Tribunal de Justiça - Unidades com vínculo direto";
+
+            for (var i = 0; i < binaryArray.Length; i++)
             {
-                if (orgao.codigo.ToLower() == cod.ToLower())
-                {
-                    watch.Stop();
-                    var elapsedMs = watch;
-                    return Ok(new { time = elapsedMs});
-                }
+              var watch2 = System.Diagnostics.Stopwatch.StartNew();
+              
+              var index = orgaos.BinarySearch(teste, new OrgaoComparer());
+              watch2.Stop();
+              if (index < 0)
+              {
+                  return NotFound();
+              }
+              var elapsedMs = watch2.Elapsed.TotalMilliseconds;
+              binaryArray[i] = elapsedMs;
             }
 
-            return NotFound();
+            for (var i = 0; i < linearArray.Length ; i++)
+            {
+              var watch = System.Diagnostics.Stopwatch.StartNew();
+              foreach (Orgao orgao in orgaos)
+              {
+                  if (orgao.codigo.ToLower() == teste.codigo.ToLower() && orgao.descricao.ToLower() == teste.descricao.ToLower())
+                  {
+                      watch.Stop();
+                      var elapsedMs = watch.Elapsed.TotalMilliseconds;
+                      linearArray[i] = elapsedMs;
+                  }
+              }
+            }
+            
+            
+
+            return Ok(new { linear = linearArray, binary = binaryArray });
+            
         }
 
         [HttpGet]
-        [Route("GetAll")]
-        public async Task<ActionResult<IEnumerable<Orgao>>> GetALL()
-        {
-            List<Orgao> orgaos = null ;
-
-
-
-            var client = new HttpClient();
-
-            client.DefaultRequestHeaders.Add("chave-api-dados", "4bda51885581e6837f94ba4e6d34e710");
-
-
-
-            var streamTask = client.GetStreamAsync("http://api.portaldatransparencia.gov.br/api-de-dados/orgaos-siafi?pagina=1");
-
-            orgaos = await JsonSerializer.DeserializeAsync<List<Orgao>>(await streamTask);
-
-            return orgaos;
-        }
-
-        [HttpGet]
-        [Route("Binary")]
-        public async Task<ActionResult<Orgao>> GetBinary()
+        [Route("all")]
+        public async Task<ActionResult<Orgao>> GetAll()
         {
             List<Orgao> orgaos = null;
-            var teste = new Orgao();
-            teste.codigo = "01000";
-            teste.descricao = "Câmara dos Deputados - Unidades com vínculo direto";
-
 
             var client = new HttpClient();
 
             client.DefaultRequestHeaders.Add("chave-api-dados", "4bda51885581e6837f94ba4e6d34e710");
 
 
-
             var streamTask = client.GetStreamAsync("http://api.portaldatransparencia.gov.br/api-de-dados/orgaos-siafi?pagina=1");
 
             orgaos = await JsonSerializer.DeserializeAsync<List<Orgao>>(await streamTask);
-            var watch = System.Diagnostics.Stopwatch.StartNew();
             
-            var index = orgaos.BinarySearch(teste, new OrgaoComparer());
-            watch.Stop();
-            if (index < 0)
-            {
-                return NotFound();
-            }
-           
-            
-            var elapsedMs = watch;
-   
 
-            return Ok(new { time = elapsedMs});
+            
+
+            return Ok(new { orgaos });
+            
         }
     }
 }
--- Alg/Startup.cs
+++ Alg/Startup.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -7,10 +11,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Alg
 {
@@ -26,11 +26,12 @@ namespace Alg
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddControllers().AddNewtonsoftJson();
+            services.AddCors();
+            services.AddControllers();
+          
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Alg", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "AppDotnet", Version = "v1" });
             });
         }
 
@@ -41,12 +42,18 @@ namespace Alg
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Alg v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AppDotnet v1"));
             }
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseCors(builder => builder
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .SetIsOriginAllowed((host) => true)
+                .AllowCredentials()
+            );
 
             app.UseAuthorization();
 
